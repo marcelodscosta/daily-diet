77aff669a21408937240a922f55bccd5e0f76b04
Marcelo Costa <marcelodscosta@yahoo.com.br>
2023-05-19 21:56:27 -0300

    Installing and loading fonts theme building

--- App.tsx
+++ App.tsx
@@ -1,10 +1,27 @@
-import { Text, View, StatusBar } from "react-native";
+import { ThemeProvider } from "styled-components/native";
+import theme from "./src/theme";
+import {
+  useFonts,
+  NunitoSans_400Regular,
+  NunitoSans_700Bold,
+} from "@expo-google-fonts/nunito-sans";
+
+import { Text, StatusBar, ActivityIndicator } from "react-native";
 
 export default function App() {
+  const [fontloaded] = useFonts({
+    NunitoSans_400Regular,
+    NunitoSans_700Bold,
+  });
+
   return (
-    <View>
+    <ThemeProvider theme={theme}>
       <StatusBar />
-      <Text>Projeto Inicializado!!!</Text>
-    </View>
+      {fontloaded ? (
+        <Text>Projeto Inicializado!!!</Text>
+      ) : (
+        <ActivityIndicator />
+      )}
+    </ThemeProvider>
   );
 }
