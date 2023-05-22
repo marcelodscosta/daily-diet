75923700e64d66e05dc3cc2d8a27898eef56192b
Marcelo Costa <marcelodscosta@yahoo.com.br>
2023-05-22 15:42:04 -0300

    Finalizing statistics details

--- src/screens/StatitisticsDetails/index.tsx
+++ src/screens/StatitisticsDetails/index.tsx
@@ -1,11 +1,19 @@
 import {
   Container,
+  GeneralNumbers,
+  GeneralNumbersDetails,
+  GeneralNumbersDetails1,
+  GeneralStatistics,
   Header,
   Icon,
   Percentage,
   SubTitle,
+  SubTitleGeneralNumbers,
+  TextGeneralNumbers,
+  Title,
   Wrap,
   WrapIcon,
+  Wraper,
 } from "./styled";
 
 export const StatisticsDetails = () => {
@@ -20,6 +28,38 @@ export const StatisticsDetails = () => {
         <Percentage>90.86%</Percentage>
         <SubTitle>das refeições dentro da dieta</SubTitle>
       </Header>
+
+      <GeneralStatistics>
+        <Title>Estatísticas Gerais</Title>
+
+        <GeneralNumbers>
+          <TextGeneralNumbers>4</TextGeneralNumbers>
+          <SubTitleGeneralNumbers>
+            Melhor Sequência de pratos dentro da dieta
+          </SubTitleGeneralNumbers>
+        </GeneralNumbers>
+
+        <GeneralNumbers>
+          <TextGeneralNumbers>109</TextGeneralNumbers>
+          <SubTitleGeneralNumbers>refeições registradas</SubTitleGeneralNumbers>
+        </GeneralNumbers>
+
+        <Wraper>
+          <GeneralNumbersDetails>
+            <TextGeneralNumbers>32</TextGeneralNumbers>
+            <SubTitleGeneralNumbers>
+              refeições dentro da dieta
+            </SubTitleGeneralNumbers>
+          </GeneralNumbersDetails>
+
+          <GeneralNumbersDetails1>
+            <TextGeneralNumbers>77</TextGeneralNumbers>
+            <SubTitleGeneralNumbers>
+              refeições fora da dieta
+            </SubTitleGeneralNumbers>
+          </GeneralNumbersDetails1>
+        </Wraper>
+      </GeneralStatistics>
     </Container>
   );
 };
--- src/screens/StatitisticsDetails/styled.ts
+++ src/screens/StatitisticsDetails/styled.ts
@@ -13,6 +13,7 @@ export const Container = styled.View`
 export const Header = styled(View)<DietStatisticStyleProps>`
   width: 100%;
   height: 200px;
+  margin-bottom: -10px;
   background-color: ${({ theme, type }) =>
     type === "PRIMARY" ? theme.COLORS.GREEN_LIGHT : theme.COLORS.RED_LIGHT};
   justify-content: center;
@@ -48,4 +49,80 @@ export const SubTitle = styled.Text`
     font-family: ${theme.FONT_FAMILY.REGULAR};
     font-size: ${theme.FONT_SIZE.BODY_S}px;
   `};
+`;
+
+export const GeneralStatistics = styled.View`
+  width: 100%;
+  height: 644px;
+  margin-top: -20px;
+  background-color: ${({ theme }) => theme.COLORS.GRAY_700};
+  border-top-left-radius: 30px;
+  border-top-right-radius: 30px;
+
+  align-items: center;
+  padding-top: 32px;
+`;
+
+export const Title = styled.Text`
+  ${({ theme }) => css`
+    color: ${theme.COLORS.GRAY_100};
+    font-family: ${theme.FONT_FAMILY.BOLD};
+    font-size: ${theme.FONT_SIZE.TITLE_XS}px;
+  `};
+  margin-bottom: 12px;
+`;
+
+export const GeneralNumbers = styled.View`
+  width: 90%;
+  height: 89px;
+  background-color: ${({ theme }) => theme.COLORS.GRAY_600};
+  justify-content: center;
+  align-items: center;
+  border-radius: 8px;
+  margin-top: 12px;
+`;
+
+export const TextGeneralNumbers = styled.Text`
+  ${({ theme }) => css`
+    color: ${theme.COLORS.GRAY_100};
+    font-family: ${theme.FONT_FAMILY.BOLD};
+    font-size: ${theme.FONT_SIZE.TITLE_M}px;
+  `};
+`;
+
+export const SubTitleGeneralNumbers = styled.Text`
+  ${({ theme }) => css`
+    color: ${theme.COLORS.GRAY_200};
+    font-family: ${theme.FONT_FAMILY.REGULAR};
+    font-size: ${theme.FONT_SIZE.BODY_S}px;
+  `};
+`;
+
+export const Wraper = styled.View`
+  width: 90%;
+  height: 89px;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 12px;
+  flex-direction: row;
+`;
+
+export const GeneralNumbersDetails = styled.View`
+  width: 48%;
+  height: 89px;
+  background-color: ${({ theme }) => theme.COLORS.GRAY_600};
+  justify-content: center;
+  align-items: center;
+  border-radius: 8px;
+  margin-top: 12px;
+`;
+
+export const GeneralNumbersDetails1 = styled.View`
+  width: 48%;
+  height: 89px;
+  background-color: ${({ theme }) => theme.COLORS.RED_LIGHT};
+  justify-content: center;
+  align-items: center;
+  border-radius: 8px;
+  margin-top: 12px;
 `;
